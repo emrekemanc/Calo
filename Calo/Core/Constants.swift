7a0b90efd819453c086d6f1eef11746666e40346
Muhammet Emre Kemancı <k.emre.k25@gmail.com>
2025-08-06 15:57:29 +0300

    Added Color set and Constants

---
+++ Calo/Core/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constracts.swift
+//  Calo
+//
+//  Created by Muhammet Emre Kemancı on 6.08.2025.
+//
+
+import SwiftUI
+
+struct Constants {
+    struct Colors{
+        static let primary = Color("PrimaryColor")
+        static let secondary = Color("SecondaryColor")
+        static let tertiary = Color("TertiaryColor")
+        static let quaternary = Color("QuaternaryColor")
+    }
+    struct LocalizationKeys{
+        
+    }
+    
+}

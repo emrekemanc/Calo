b888dbc136b7da594ca414cb4369ebddb6b562d9
Muhammet Emre Kemancı <k.emre.k25@gmail.com>
2025-08-06 09:59:00 +0300

    Initial Commit

---
+++ Calo/CaloApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloApp.swift
+//  Calo
+//
+//  Created by Muhammet Emre Kemancı on 6.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CaloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calo
+//
+//  Created by Muhammet Emre Kemancı on 6.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
